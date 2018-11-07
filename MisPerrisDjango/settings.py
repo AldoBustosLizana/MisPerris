ccfb63ea6009e0790c64a8b154d6ef62b2db7729
Jorge <jorge_BFMV_14@hotmail.com>
2018-11-07 12:58:47 -0300

    usuario login

--- MisPerrisDjango/settings.py
+++ MisPerrisDjango/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy
+from django.contrib.auth import login
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +124,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
