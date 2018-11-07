4aafa77e584d029d10129ed78f3f71133099e125
Jorge <jorge_BFMV_14@hotmail.com>
2018-11-07 16:10:41 -0300

    jjj

--- MisPerrisDjango/urls.py
+++ MisPerrisDjango/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.conf.urls import url,include
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
 from django.urls import path
 
 urlpatterns = [
@@ -10,5 +10,10 @@ urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
     url(r'^perro/', include ('Apps.perro.urls' )),
     url(r'^usuario/',include('Apps.usuarios.urls')),
+    url(r'^reset/password_reset',PasswordResetView, {'template_name':'registration/password_reset_form.html',
+    'email_template':'registration/password_reset_email.html'}),
+    url(r'^reset/password_reset_done',PasswordResetDoneView,{'template_name': 'registration/password_reset_done.html'}),
+    url(r'^reset/(?P<uidba64>[0-9A-Za-z_\-]+)/(?P<token>.+)$', PasswordResetConfirmView,{'template_name':'registration/password_reset_confirm.html'}),
+    url(r'^reset/done',PasswordResetCompleteView,{'template_name': 'registration/password_reset_complete.html'})
 ]
 
