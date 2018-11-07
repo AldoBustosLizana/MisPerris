fe28f688221b850017b3593e6a01befa6cc47838
Jorge <jorge_BFMV_14@hotmail.com>
2018-11-07 13:13:29 -0300

    avance

--- MisPerrisDjango/urls.py
+++ MisPerrisDjango/urls.py
@@ -1,12 +1,12 @@
 
 from django.contrib import admin
 from django.conf.urls import url,include
-from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^perro/', include ('Apps.perro.urls')),
+    url(r'^perro/', include ('Apps.perro.urls' )),
     url(r'^usuario/',include('Apps.usuarios.urls')),
-    url(r'^$',login,{'template_name':'login.html'})
+    url(r'^$',LoginView,{'template_name':'login.html'}, name="login")
 ]
 
