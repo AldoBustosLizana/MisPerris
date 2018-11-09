5b0d023d14a929fc0de37d93631422ee59ba0a3d
Jorge <jorge_BFMV_14@hotmail.com>
2018-11-09 15:07:40 -0300

    version 1.1 jorge

--- Apps/perro/urls.py
+++ Apps/perro/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import views
 
-from . import views
 
 urlpatterns = [
     path('home', views.index, name='index'),
